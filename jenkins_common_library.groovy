6b4aab38c6fbcb9331289c506cf71a6032b8eacc
vilvamani.arumugam <vilvamani.arumugam@accenture.com>
2020-11-16 19:42:10 +0530

    updated jenkins file

--- jenkins_common_library.groovy
+++ jenkins_common_library.groovy
@@ -200,9 +200,11 @@ def deployToKubernetes(configs) {
     dir(configs.branch_checkout_dir) {
         withKubeConfig(credentialsId: kubernetes_credentials_id, serverUrl: kubernetes_url) {
 
-            sh "sed -i 's|DOCKER_REPO|${configs.dockerRepoName}|g' infra/k8s-deployment.yaml"
             sh "sed -i 's|DOCKER_IMAGE|${configs.dockerImageName}|g' infra/k8s-deployment.yaml"
-            sh "sed -i 's|DOCKER_TAG|${configs.git_commit_id}|g' infra/k8s-deployment.yaml"
+
+            //sh "sed -i 's|DOCKER_REPO|${configs.dockerRepoName}|g' infra/k8s-deployment.yaml"
+            //sh "sed -i 's|DOCKER_IMAGE|${configs.dockerImageName}|g' infra/k8s-deployment.yaml"
+            //sh "sed -i 's|DOCKER_TAG|${configs.git_commit_id}|g' infra/k8s-deployment.yaml"
 
             sh "kubectl apply -f ${configs.kubeDeploymentFile}"
             sh "kubectl apply -f ${configs.kubeServiceFile}"
