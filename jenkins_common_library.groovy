39ebfd39d5544da755ba16b801695f6622a408d8
vilvamani.arumugam <vilvamani007@gmail.com>
2020-11-23 10:33:47 +0530

    Updated git status

--- jenkins_common_library.groovy
+++ jenkins_common_library.groovy
@@ -276,15 +276,16 @@ def getCommitSha() {
 def updateGithubCommitStatus(build) {
   // workaround https://issues.jenkins-ci.org/browse/JENKINS-38674
   repoUrl = getRepoURL()
-  COMMITSHA = getCommitSha()
+  GIT_COMMIT = getCommitSha()
 
   print("=====")
-  print(COMMITSHA)
+  print(GIT_COMMIT)
 
-sh label: '', script: '''curl \\"https://api.GitHub.com/repos/vilvamani/springboot/statuses/$COMMITSHA?access_token=977bb18a34d16cde7beb1c12be288bdf8b04cadb\\" \\
-  -H "Content-Type: application/json" \\
-  -X POST \\
-  -d "{\\"state\\": \\"success\\",\\"context\\": \\"continuous-integration/jenkins\\", \\"description\\": \\"Jenkins\\", \\"target_url\\": \\"http://10.0.212.76:30001/job/springboot-ms/$BUILD_NUMBER/console\\"}"'''
+bitbucket_api_url = "https://api.GitHub.com/repos/vilvamani/springboot/statuses/$GIT_COMMIT?access_token=5204897a651d3248bcc005e6f946b7204d6a39e5" 
+sh "curl $bitbucket_api_url\
+  -H "Content-Type: application/json" \
+  -X POST \
+  -d "{\"state\": \"success\",\"context\": \"continuous-integration/jenkins\", \"description\": \"Jenkins\", \"target_url\": \"http://10.0.212.76:30001/job/springboot-ms/$BUILD_NUMBER/console\"}""
 
 
 }
